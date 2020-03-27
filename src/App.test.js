b47adee5f14e59e4038c2ee96030a4128fe7ec88
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2020-03-26 21:34:06 -0700

    added test to deloy

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+
+test('home', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/home/i);
+  expect(linkElement).toBeInTheDocument();
+});
